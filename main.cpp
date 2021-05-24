48ab09b31cdc3cc64b9fc7f212076ec5b85b05d4
MylesTrump <>
2021-05-24 00:43:37 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 Myles Trump All rights reserved
+//
+// Created by: Myles Trump
+// Created on: May 2021
+// This program does basic math
+
+#include <iostream>
+#include <math.h>       /* pow */
+
+int main() {
+    std::cout << "Perimeter (cm) = " << (5 * 2 + 3 * 2) << std::endl;
+    std::cout << "Area (cm) = " << (5 * 3) << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Done." << std::endl;
+}
